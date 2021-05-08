53d83172a2fc5dd519e1f312c4adaa7dea2b529f
Manaday Mavani <m.k.mavani@gmail.com>
2021-05-08 19:57:58 +0530

    fix(pwa)

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -11,7 +11,7 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  new RegExp('https://manadaym.github.io/*'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
